b46543fade19dae72f4037ede43188212a601f60
dim4ik85 <dim4ik1985@yandex.ru>
2022-09-11 19:09:21 +0300

    edit app

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -20,16 +20,6 @@ DATA = {
 }
 
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
-
 def calc_ingredients(request, recipe):
     dict_new = {}
     servings = int(request.GET.get('servings', 1))
